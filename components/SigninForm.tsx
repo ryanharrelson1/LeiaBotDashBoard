315b7eec2983845bfd042a340ed5088200a69189
ryan <lostramen1998@gmail.com>
2024-10-12 16:44:15 -0500

    signin page componet

--- components/SigninForm.tsx
+++ components/SigninForm.tsx
@@ -38,11 +38,75 @@ const SigninForm = () => {
     
 
   return (
-    <div>
+    <main>
 
+    <div className="glass rounded-lg p-10 px-20">
+        <div className="flex flex-col items-center gap-2">
+          <h1 className="font-bold text-text-lilly-pad-white text-3xl">
+            Moderator Login
+          </h1>
+          <img src="froggylogonew1.png" alt="logo" height={100} width={100} />
+          <h1 className="text-text-froggie-green font-bold text-2xl">
+            Sign In
+          </h1>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="UserId"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-text-lilly-pad-white">
+                      UserId
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        className="placeholder:text-text-lilly-pad-white text-text-froggie-green rounded-xl"
+                        placeholder="UserId"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="Password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-text-lilly-pad-white">
+                      Password
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="Password"
+                        className="placeholder:text-text-lilly-pad-white text-text-froggie-green rounded-xl"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex flex-col justify-center w-full gap-2">
+                <Button
+                  type="submit"
+                  className="bg-light-petal-pink text-text-lilly-pad-white font-bold w-full"
+                >
+                  Login
+                </Button>
+                <p className="text-light-petal-pink">
+                  Forgot Login? Contact @Azn Daddy on Discord
+                </p>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
 
-
-    </div>
+    </main>
   )
 }
 
